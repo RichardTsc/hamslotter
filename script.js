a94c73ffa2d1d377a1c8e9c6ace7cd493bffa2c8
RichardTsc <RichardTsc@gmail.com>
2023-08-22 20:04:38 +0000

    update

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Initialize flatpickr for date-time picker
+flatpickr("#dateTimePicker", {
+    enableTime: true,
+    minDate: "tomorrow",
+    defaultHour: 6,
+    defaultMinute: 30,
+    minuteIncrement: 15,
+});
+
 document.getElementById('inputForm').addEventListener('submit', async function(event) {
     event.preventDefault();
     const formData = new FormData(event.target);
